6aff9c9ac642a0642e97f252fff82b3e3e2ca739
Will Jackson <greenerside71@gmail.com>
2013-05-12 17:53:07 -0400

    Forgot to do inverse transformation, done now

--- src/igl/primitive.cpp
+++ src/igl/primitive.cpp
@@ -59,16 +59,66 @@ mat4f transformed_matrix(TransformedSurface* transformed, float time) {
     auto m = mat_t * mat_r_z * mat_r_y * mat_r_x * mat_s;
 
     return m;
-    //return m * transformed_matrix_inv(transformed); // uncomment to check inv
+    //return m * transformed_matrix_inv(transformed, time); // uncomment to check inv
 
 }
 
 mat4f transformed_matrix_inv(TransformedSurface* transformed, float time) {
     auto mi = identity_mat4f;
     
-    put_your_code_here("Animated Transformations (inverse)");
-    
-    return mi;
+    auto t = transformed->translation;
+    auto r = transformed->rotation_euler;
+    auto s = transformed->scale;
+
+    if (transformed->anim_translation)
+        t += keyframed_value(transformed->anim_translation, time);
+
+    if (transformed->anim_rotation_euler)
+        r += keyframed_value(transformed->anim_rotation_euler, time);
+
+    if (transformed->anim_scale)
+        s *= keyframed_value(transformed->anim_scale, time);
+
+    // To invert, subtract instead of adding
+    auto mat_t = mat4f(vec4f(1, 0, 0, -1*t.x),
+                       vec4f(0, 1, 0, -1*t.y),
+                       vec4f(0, 0, 1, -1*t.z),
+                       vec4f(0, 0, 0, 1)
+
+                 );
+
+    // To invert, take ^-1 of scale factors
+    auto mat_s = mat4f(vec4f((1/s.x), 0, 0, 0),
+                       vec4f(0, (1/s.y), 0, 0),
+                       vec4f(0, 0, (1/s.z), 0),
+                       vec4f(0, 0, 0, 1)
+
+                 );
+
+    // To invert, take opposite of theta (only affects sin as cos is even)
+    auto mat_r_z = mat4f(vec4f(cos(r.z), sin(r.z), 0, 0),
+                         vec4f(-1*sin(r.z), cos(r.z), 0, 0),
+                         vec4f(0, 0, 1, 0),
+                         vec4f(0, 0, 0, 1)
+
+                   );
+
+    auto mat_r_y = mat4f(vec4f(cos(r.y), 0, -1*sin(r.y), 0),
+                         vec4f(0, 1, 0, 0),
+                         vec4f(sin(r.y), 0, cos(r.y), 0),
+                         vec4f(0, 0, 0, 1)
+
+                   );
+
+    auto mat_r_x = mat4f(vec4f(1, 0, 0, 0),
+                         vec4f(0, cos(r.x), sin(r.x), 0),
+                         vec4f(0, -1*sin(r.x), cos(r.x), 0),
+                         vec4f(0, 0, 0, 1)
+
+                   );
+
+    return mat_s * mat_r_x * mat_r_y * mat_r_z * mat_t;
+
 }
 
 /// Computes the pose and rest frames at time for each bone
